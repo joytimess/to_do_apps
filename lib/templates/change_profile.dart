87f33980826d2c954ee7680320eb93e72b6bac6e
joytimess <victorw7286@gmail.com>
2025-06-11 03:04:21 +0700

    final

--- lib/templates/change_profile.dart
+++ lib/templates/change_profile.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:path_provider/path_provider.dart';
